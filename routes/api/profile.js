5e8eedf58c057c9f6a3147aa2bcffdc92b2c90ab
Nick Kulway <nkulway@gmail.com>
2022-05-09 13:59:27 -0400

    added check validation to profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const auth = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -14,13 +15,34 @@ router.get("/me", auth, async (req, res) => {
       "user",
       ["name", "avatar"]
     );
-    if(!profile){
-      return res.status(400).json({ msg: 'There is no profile for this user' })
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
     }
-    res.json(profile)
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Sever Error");
+  }
+});
+
+// @route   GET api/profile/me
+// @desc    Create or update user profile
+// @access  Private
+
+router.post("/", [auth, 
+  [ 
+    check("status", "Status is required")
+      .not()
+      .isEmpty(),
+    check("skills", "Skills is required")
+      .not()
+      .isEmpty()
+  ]
+], 
+async (req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
   }
 });
 
